93bf16c0b0c9c4672f5b5c9617cf2157c2615f98
masknugget <54693275+masknugget@users.noreply.github.com>
2020-05-28 10:16:22 +0800

    Update main.cpp

--- Chapter01/Assignment_Operator_Overload/main.cpp
+++ Chapter01/Assignment_Operator_Overload/main.cpp
@@ -9,14 +9,14 @@ protected:
     string m_name;
 
 public:
-    Animal(string name) : m_name(name)
+    Animal(string name) : m_name(name)  // 初始化绑定在m_name上
     {
 
     }
 
     // The interface that has to be implemented
     // in derived class
-    virtual string MakeSound() = 0;
+    virtual string MakeSound() = 0;   // 这是一个基类的东东， virtual mehtod implent by suclass
 
     string GetName()
     {
@@ -25,19 +25,19 @@ public:
 
 };
 
-class Dog : public Animal
+class Dog : public Animal       //继承 public Animal 
 {
 public:
     // Forward the constructor arguments
     Dog(string name) : Animal(name) {}
 
     // Copy assignment operator overloading
-    void operator = (const Dog &D ) {
+    void operator = (const Dog &D ) {              //这是个神马鬼
          m_name = D.m_name;
       }
 
     // here we implement the interface
-    string MakeSound() override
+    string MakeSound() override               // override 写在后面
     {
         return "woof-woof!";
     }
